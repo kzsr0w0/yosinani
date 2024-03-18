091f5acb5c5f2b1574d0cb1dc887a4afb3b16dc9
kzsr0w0 <wkym0508@example.com>
2024-03-18 22:43:13 +0900

    streamlitだけで動作

--- st_full.py
+++ st_full.py
@@ -1,8 +1,11 @@
 
 
+import csv
+import streamlit as st
+import random
 
 #---悩み相談入力-------------------------------------------------------------------#
-import streamlit as st
+
 
 # タイトルの設定
 st.title('悩み相談アプリ')
@@ -26,28 +29,57 @@ def generate_response(user_input):
 if user_input:
     input_text = generate_response(user_input)
 #    st.write(response)
-# Streamlitクライアントサイド
-import streamlit as st
-import requests
-import random
+    # リクエストデータのためのPydanticモデル
+    # このモデルは、クライアントから受け取るデータの構造を定義します
 
-#st.title('口調変換アプリ')
+
+    
+#辞書を作成   
+def read_csv_to_dict(file_path):
+    style_dict = {}
+    with open(file_path, mode='r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # ヘッダー行をスキップ
+        for row in reader:
+            style, key, value = row
+            if style not in style_dict:
+                style_dict[style] = {}
+            style_dict[style][key] = value
+    return style_dict
+
+# CSVファイルパス
+file_path = '口癖辞書.txt'
+
+# CSVファイルを読み込んで辞書に変換
+style_dict = read_csv_to_dict(file_path)
+
+#style_dict = {
+    #"スタイル1": {"こんにちは": "やあ！", "ありがとう": "サンキュー！"},
+    #"スタイル2": {"こんにちは": "こんにちはなのです", "ありがとう": "感謝するのです"}   
+#}
+
+def convert_text(input_text, style):
+    for key, value in style_dict[style].items():
+        input_text = input_text.replace(key, value)
+    return input_text
+
+
 
 #input_text = st.text_area("メッセージを入力してください")
 style = st.selectbox("スタイルを選択してください", ["スタイル1", "スタイル2", "スタイル3"])
 if st.button('相談'):
-    response = requests.post("http://localhost:8000/convert/", json={"text": input_text, "style": style})
-    if response.status_code == 200:
-        st.write(response.json()['converted_text'])
+    #response = requests.post("http://127.0.0.1:8000/convert/", json={"text": input_text, "style": style})
+    converted_text = convert_text(input_text, style)
+    st.write(converted_text)
 
     #名言
-    f = open('C:\\folder\\make\\Python\\yosinani\\名言.txt','r',encoding='UTF-8')
+    f = open('名言.txt','r',encoding='UTF-8')
     meigenn_list = f.readlines()
     meigenn = random.choice(meigenn_list)
     st.write('今日の名言は「',meigenn,'」です')
 
     # 最後の挨拶
-    g = open('C:\\folder\\make\\Python\\yosinani\\最後の挨拶.txt','r',encoding='UTF-8')
+    g = open('最後の挨拶.txt','r',encoding='UTF-8')
     aisatu_list = g.readlines()
     ran_list = [0, 1]
     w = [1, 5]
